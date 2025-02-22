c9753cf1a6bb22913262434571acafe4f8aa10ce
sedin-aboorva <aboorva@sedintechnologies.com>
2025-02-22 15:44:05 +0530

    Association added and migrations done

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -1,2 +1,4 @@
 class Invoice < ApplicationRecord
+  has_many :products_to_invoice, dependent: :destroy
+  has_many :products, through: :products_to_invoice
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,4 @@
 class Product < ApplicationRecord
+  has_many :products_to_invoice, dependent: :destroy
+  has_many :invoices, through: :products_to_invoice
 end
