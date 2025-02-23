9e34bbdb0834b2c393ef33761f55e1929b7dea0a
sedin-aboorva <aboorva@sedintechnologies.com>
2025-02-23 17:12:56 +0530

    Product create, edit actions with UI changes

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,9 @@
 class Product < ApplicationRecord
   has_many :products_to_invoice, dependent: :destroy
   has_many :invoices, through: :products_to_invoice
+  scope :active, -> { where(deleted_at: nil) }
+
+  def soft_delete
+    update(deleted_at: Time.current)
+  end
 end
